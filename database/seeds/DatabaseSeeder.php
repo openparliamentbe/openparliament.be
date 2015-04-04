edaa14b25750e52ecc24d3e6dd7245a264db42f7
michael <michael@estsurinter.net>
2015-04-04 20:47:04 +0200

    [db] Add seeder for documents table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call('LegislaturesTableSeeder');
         $this->call('SessionsTableSeeder');
         $this->call('DossiersTableSeeder');
+        $this->call('DocumentsTableSeeder');
     }
 }
