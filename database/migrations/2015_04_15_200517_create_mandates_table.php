1eb4dba8d58fb572a7c3e5b5e669fee49ede84fd
michael <michael@estsurinter.net>
2015-04-16 01:28:43 +0200

    Add more comments to migration for mandates table

--- database/migrations/2015_04_15_200517_create_mandates_table.php
+++ database/migrations/2015_04_15_200517_create_mandates_table.php
@@ -28,10 +28,17 @@ class CreateMandatesTable extends Migration
             $table->unsignedInteger('politician_id');
 
             // Main data.
+            // The type of the political mandate.
             $table->enum('type', ['k', 's']);
+            // The lang associated with the mandate. Usually the same as the
+            // language of the politician, but can be different.
             $table->enum('lang', ['de', 'fr', 'nl']);
+            // ID of the mandate (or the related politician) in the original
+            // data source (official website).
             $table->string('original_identifier')->nullable();
+            // E-mail address associated with the mandate.
             $table->string('email')->nullable();
+            // Date range defining the validity period of the mandate.
             $table->date('started_on');
             $table->date('ended_on')->nullable();
 
