4e6ca6a7ee71a25db6edca8597f53ac2bb2bf702
michael <michael@estsurinter.net>
2015-04-22 13:16:55 +0200

    Generate currently valid K mandates

--- database/seeds/MandatesTableSeeder.php
+++ database/seeds/MandatesTableSeeder.php
@@ -14,7 +14,12 @@ class MandatesTableSeeder extends Seeder
      */
     public function run()
     {
-        $data = $this->getHardcodedData();
+        // Start by adding a few hardcoded mandates.
+        // Then, add a bunch of generated ones.
+        $data = array_merge(
+            $this->getHardcodedData(),
+            $this->generateCurrentChamberMandates()
+        );
 
         DB::table('mandates')->insert($data);
     }
@@ -58,5 +63,47 @@ class MandatesTableSeeder extends Seeder
                 'ended_on'            => null,
             ],
         ];
+    }
+
+    /**
+     * Generate an array of currently valid mandates for the Chamber.
+     *
+     * @return array
+     */
+    protected function generateCurrentChamberMandates()
+    {
+        $data = [];
+        $counter = 4;
+
+        foreach (range(4, 500) as $index) {
+            // We will leave some politicians without any mandate.
+            if (
+                // Skipped NL politicians
+                $index > 92 && $index <= 300
+                // Skipped FR politicians
+                || $index > 356 && $index <= 480
+                // Skipped DE politicians
+                || $index > 482
+            ) {
+                continue;
+            }
+
+            $lang = 'nl';
+            if ($index > 300) $lang = 'fr';
+            if ($index > 480) $lang = 'de';
+
+            $data[] = [
+                'legislature_id'      => '54k',
+                'politician_id'       => $index,
+                'type'                => 'k',
+                'lang'                => $lang,
+                'original_identifier' => null,
+                'email'               => 'mandate'.($counter++).'@lachambre.dev',
+                'started_on'          => '2014-06-19',
+                'ended_on'            => null,
+            ];
+        }
+
+        return $data;
     }
 }
